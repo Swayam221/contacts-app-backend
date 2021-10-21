b9cfbbd1af868e83e2cc4ebc502340b9f8c44134
Swayam221 <68892315+Swayam221@users.noreply.github.com>
2021-10-21 10:53:41 +0530

    created server file

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = 7000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//import routes
+
+//db
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL, {
+        //   useCreateIndex: true,
+          useUnifiedTopology: true,
+        //   useFindAndModify: false,
+          useNewUrlParser: true,
+        });
+        console.log("connected to db");
+      } catch (error) {
+        console.log(error);
+        process.exit(1);
+      }
+}
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Contacts App');
+})
+
+app.listen(process.env.PORT || port);
